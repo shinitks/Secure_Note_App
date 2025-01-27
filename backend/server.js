bcd2cf1f7e82b0ba817e4002cd5066e80140cfee
shinit <shinitks7@gmail.com>
2025-01-27 11:54:08 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 
 
-mongoose.connect('mongodb+srv://shinitks:sh1n1tks%237@cluster0.3vpmi.mongodb.net/myDatabaseName?retryWrites=true&w=majority', {
+mongoose.connect(process.env.USERS_DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
@@ -16,6 +16,4 @@ app.listen(port,()=>{
         console.log('server started');
     });
 
-    //mongodb+srv://shinitks:sh1n1tks%237@cluster0.3vpmi.mongodb.net/myDatabaseName?retryWrites=true&w=majority
-    //mongodb+srv://shinitks:sh1n1tks%237@cluster0.3vpmi.mongodb.net/sessionDB?retryWrites=true&w=majority
-    //mongodb://127.0.0.1:27017/myDatabaseName
+   
