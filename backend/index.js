6b44047432756b4e2bb070fa8d38ca2285ca7920
shinit <shinitks7@gmail.com>
2025-01-30 11:00:19 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const sessionConfig = require('./config/sessionConfig');
 
 app.use(
   cors({
-    origin: 'https://secure-note-app-frontend.onrender.com',
+    origin: 'https://secure-note-app-frontend-h7vi.onrender.com',
     credentials: true, 
     allowedHeaders: ['Authorization', 'X-csrf-token', 'Content-Type', 'Cookie'],
   })
